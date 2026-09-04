d5125c1039e1ee4919e997633a611856819cc4fe
shehan-wijekoon <shehan.wijekoon@gmail.com>
2026-09-04 10:35:55 +0530

    enable CORS for image load

--- admin-panel/next.config.ts
+++ admin-panel/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allows external avatar images from any HTTPS domain
+      },
+      {
+        protocol: "http",
+        hostname: "**", // Optional: Allows HTTP images (useful during local testing)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
